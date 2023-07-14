58733295debb4b4deb0582d758096c4de717bf29
kalpeshbhadane185 <kbhadane@logilite.com>
2023-07-14 20:26:43 +0530

    First Commit

---
+++ CSV_Utility/src/main/java/com/logilite/CSV_Utility/CSV_Utility_Logger.java
@@ -0,0 +1,18 @@
+package com.logilite.CSV_Utility;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class CSV_Utility_Logger
+{
+
+	public static Logger logger = LogManager.getLogger(CSV_Utility_Logger.class);
+
+	static
+	{
+		PropertyConfigurator.configure(
+				System.getProperty("user.home") + System.getProperty("file.separator") 
+				+ "/log4j.properties");
+	}
+}
